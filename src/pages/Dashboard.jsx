5e05f1686e1de72ff97ab6a4a68afbab850b91d8
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-02-05 09:59:20 -0600

    Initial Commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Heading>Chakra UI Components</Heading>
+      <Text>Hello from the USA.</Text>
+    </Container>
+  );
+}
