3342f323b3aead155229f6f45f5e4dafbfb38581
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-02-05 10:52:05 -0600

    Dashboard styling props testing updates.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,34 @@
-import { Container, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
+  const boxstyles = {
+    p: "10px",
+    bg: "purple.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    filter: "blur(2px)",
+    ":hover": {
+      color: "black",
+      bg: "blue.200",
+    },
+  };
+
   return (
-    <Container>
-      <Heading>Chakra UI Components</Heading>
-      <Text>Hello from the USA.</Text>
+    <Container as="section" maxW="4xl" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra UI Components
+      </Heading>
+      <Text ml="30px">Hello from the USA.</Text>
+      <Text ml="30px" color="blue.500" fontWeight="bold">
+        Hello from the USA.
+      </Text>
+
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a box</Text>
+      </Box>
+
+      <Box sx={boxstyles}>Hello World!</Box>
     </Container>
   );
 }
